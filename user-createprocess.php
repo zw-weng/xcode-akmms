d710d09d3a2c34399b488f8d2a6b4cf24ec1e784
Neo Zheng Weng <118497269+zw-weng@users.noreply.github.com>
2023-12-31 14:56:04 +0800

    Update user-createprocess.php

--- user-createprocess.php
+++ user-createprocess.php
@@ -1,5 +1,6 @@
 <?php
-// Connect to DB
+// Include neccessary files
+include('mysession.php');
 include('dbconnect.php');
 include('function.php');
 
@@ -11,20 +12,20 @@ if(isset($_POST['saveuser'])){
     $fpwd = validate($_POST['fpwd']);
     $fphone = validate($_POST['fphone']);
     $femail = validate($_POST['femail']);
-    $fadd = validate($_POST['fadd']);
-    $fposition = validate($_POST['fposition']);
     $ftype = validate($_POST['ftype']);
 
-    if($fid != '' || $fic != '' || $ffname != '' || $flname != '' || $fpwd != '' || $fphone != '' || $femail != '' || $fadd != '' || $fposition != '' || $ftype != ''){
+    if($fid != '' || $fic != '' || $ffname != '' || $flname != '' || $fpwd != '' || $fphone != '' || $femail != '' || $ftype != ''){
+        $hashedPassword = password_hash($fpwd, PASSWORD_BCRYPT);
+
         // Use prepared statement to prevent SQL injection
-        $sql = "INSERT INTO tb_user(user_id, user_ic, user_pwd, u_fName, u_lName, user_phone, user_email, user_address, user_position, type_id)
-                VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+        $sql = "INSERT INTO tb_user(user_id, user_ic, user_pwd, u_fName, u_lName, user_phone, user_email, type_id)
+                VALUES(?, ?, ?, ?, ?, ?, ?, ?)";
 
         $stmt = mysqli_prepare($con, $sql);
 
         if ($stmt) {
             // Bind parameters
-            mysqli_stmt_bind_param($stmt, "ssssssssss", $fid, $fic, $fpwd, $ffname, $flname, $fphone, $femail, $fadd, $fposition, $ftype);
+            mysqli_stmt_bind_param($stmt, "ssssssss", $fid, $fic, $hashedPassword, $ffname, $flname, $fphone, $femail, $ftype);
 
             // Execute the statement
             $result = mysqli_stmt_execute($stmt);
